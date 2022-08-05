1297cdeb9085323c9c65faa00ac8ee7e20e6b437
Ivan Borovskiy <iborovskiy@gmail.com>
2022-08-05 16:22:10 +0300

    Create ETL Pipeline

--- etl.py
+++ etl.py
@@ -1,5 +1,6 @@
 import os
 import glob
+import datetime
 import psycopg2
 import pandas as pd
 from sql_queries import *
@@ -7,37 +8,37 @@ from sql_queries import *
 
 def process_song_file(cur, filepath):
     # open song file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # insert song record
-    song_data = 
-    cur.execute(song_table_insert, song_data)
-    
+    song_data = df[["song_id", "title", "artist_id", "year", "duration"]].values
+    cur.execute(song_table_insert, list(song_data[0]))
+
     # insert artist record
-    artist_data = 
+    artist_data = df[["artist_id", "artist_name", "artist_location", "artist_latitude", "artist_longitude"]].iloc[0].values.tolist()
     cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
     # open log file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # filter by NextSong action
-    df = 
+    df = df[df["page"] == 'NextSong']
 
     # convert timestamp column to datetime
-    t = 
-    
+    t = df['ts'].apply(lambda x: datetime.datetime.fromtimestamp(x/1000.0))
+
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    time_data = [*zip(t, t.dt.hour, t.dt.day, t.dt.isocalendar().week, t.dt.month, t.dt.year, t.dt.weekday)]
+    column_labels = ['start_time', 'hour', 'day', 'week', 'month', 'year', 'weekday']
+    time_df = pd.DataFrame(dict(zip(column_labels,zip(*time_data))))
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = 
+    user_df = df[['userId', 'firstName', 'lastName', 'gender', 'level']]
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -45,18 +46,18 @@ def process_log_file(cur, filepath):
 
     # insert songplay records
     for index, row in df.iterrows():
-        
+
         # get songid and artistid from song and artist tables
         cur.execute(song_select, (row.song, row.artist, row.length))
         results = cur.fetchone()
-        
+
         if results:
             songid, artistid = results
         else:
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = 
+        songplay_data = (t.loc[index], row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
         cur.execute(songplay_table_insert, songplay_data)
 
 
